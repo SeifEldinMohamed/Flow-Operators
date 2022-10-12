46f4b295068e75c5c9c8b35a7729f852ee8a63f4
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-12 07:39:21 +0200

    collect and collectLatest

--- app/src/main/java/com/seif/flowoperators/MainActivity.kt
+++ app/src/main/java/com/seif/flowoperators/MainActivity.kt
@@ -3,13 +3,16 @@ package com.seif.flowoperators
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.seif.flowoperators.ui.theme.FlowOperatorsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +20,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FlowOperatorsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val viewModel = viewModel<MainViewModel>()
+                val count = viewModel.countDownFlow.collectAsState(initial = 0)
+
+                Box(modifier = Modifier.fillMaxSize()) {
+                        Text(text = "Counter: ${count.value}")
                 }
             }
         }
