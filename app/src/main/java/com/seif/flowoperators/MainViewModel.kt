6a0e4b578630e6fbd608052bbc5baaafbca3e2ba
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-12 07:48:54 +0200

    Filter, Map and OnEach

--- app/src/main/java/com/seif/flowoperators/MainViewModel.kt
+++ app/src/main/java/com/seif/flowoperators/MainViewModel.kt
@@ -24,12 +24,28 @@ class MainViewModel() : ViewModel() {
     }
 
     private fun collectFlow() {
+//        countDownFlow.onEach {
+//            println(it)
+//        }.launchIn(viewModelScope)
+
         viewModelScope.launch {
-            countDownFlow.collect() { time ->
-                delay(1500L)
+            countDownFlow
+                .filter { time ->
+                    time % 2 == 0 // receive even values
+                }
+                .map { time ->
+                    time * time
+                }
+                .onEach { time -> // it doesn't transform our values instead it does something with these
+                    println(time)
+                } // returns flow
+                .collect() { time ->
                 println("the current time is $time")
             }
         }
     }
 
 }
+// collect: emit every change happened even it takes longer time than emition
+// collectLatest: emit every change put if the work done in collector takes more time than emition then it will cancel this coroutine and collect the latest one
+// this can be used in case of we update the ui then we need the latest update no matter if there is an older change not shown
