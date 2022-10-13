ab2cbf0f39d2ed42352431ef1dafbe4221efd94c
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-13 07:48:20 +0200

    Flattening Operators

--- app/src/main/java/com/seif/flowoperators/MainViewModel.kt
+++ app/src/main/java/com/seif/flowoperators/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.seif.flowoperators
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -57,6 +58,43 @@ class MainViewModel() : ViewModel() {
         }
     }
 
+    @FlowPreview
+    private fun collectFlow2(){
+        val flow1 = flow {
+            emit(1)
+            delay(500L)
+            emit(2)
+        }
+        viewModelScope.launch {
+            flow1.flatMapConcat { value ->
+                flow {
+                    emit(value + 1)
+                    delay(500L)
+                    emit(value + 2)
+                }
+            }.collect{ value ->
+                println("the value is : $value")
+
+            }
+        }
+    }
+
+    @FlowPreview
+    private fun collectFlow3(){
+        val flow1 = (1..5).asFlow()
+        viewModelScope.launch {
+            flow1.flatMapConcat { id ->
+           //  getRecipesId(id) // this function will accessed the cache and return a flow
+                flow {
+                    emit(id)
+                }
+            }.collect{ value ->
+                println("the value is : $value")
+
+            }
+        }
+    }
+
 }
 // collect: emit every change happened even it takes longer time than emition
 // collectLatest: emit every change put if the work done in collector takes more time than emition then it will cancel this coroutine and collect the latest one
@@ -66,3 +104,10 @@ class MainViewModel() : ViewModel() {
 // reduce:  as we can accumulate all values in list (accumulator: at first will be the first value then it will be the addition of it and value, value: next emission)
 // fold : same as reduce but it has initial value
 // count:  it will count the values match a specific condition
+
+// flattening: if we have list of lists then we want to have a single list of all entries
+// [[1,2], [1,2,3]] -> [1,2,1,2,3]
+
+// flatMapConcat: will finish this flow's emission and then actually go with id 2 finish id 2 emission and so on
+// flatMapMerge: will do all that at the same time it won't wait the first emission to finish it will directly launch the flow for id 1,2,3 and so on
+// flatMapLatest: it will drop the flow that working if another flow emits
